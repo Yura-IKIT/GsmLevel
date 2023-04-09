a4adcdcdded68a3a7866172e2dd9f0ba3d0a7bf5
YuMat <yumat1952@gmail.com>
2023-04-09 12:57:30 +0300

    fl=listdir

--- GetData/ReadTextFile.py
+++ GetData/ReadTextFile.py
@@ -1,10 +1,12 @@
-
+#branch about
 import os
 print("read ASCI file with GSM data")
 with open('18.09.22.TXT', 'r') as f:
     data = f.read()
     print(data)
-f=os.listdir()
-print(f)
-print(f[1])
-print(f[-1])
+f.close()
+
+fl=os.listdir()
+print(fl)
+print('fl[1]=',fl[1])
+print('fl[-1]=',fl[-1])
